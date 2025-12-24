45a77404d1b6d18d753e3b6569aaa4b6ec9cdb4f
Tosin Adedokun <109170276+Tosin-A@users.noreply.github.com>
2025-12-24 22:24:59 +0000

    Fix base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/<Coresense---Cora>/',
   plugins: [
-    base: '/<Coresense---Cora>/',
+    
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
